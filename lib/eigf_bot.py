a7c82e810316ca37483c8450af5bf6662f1fdf73
RileyMKDixon <31675727+RileyMKDixon@users.noreply.github.com>
2019-04-29 16:58:49 -0600

    Organize the modules and docs

---
+++ lib/eigf_bot.py
@@ -0,0 +1,14 @@
+import os
+import asyncio
+
+import discord
+
+class EIGFBot():
+    def __init__():
+        pass
+
+    def startup():
+        pass
+
+    def close():
+        pass
--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-import asyncio
-
-import discord
-
