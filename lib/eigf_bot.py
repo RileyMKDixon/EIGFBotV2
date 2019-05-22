37792802284d68c50c56824cca95a0810f6c7359
RileyMKDixon <31675727+RileyMKDixon@users.noreply.github.com>
2019-05-21 22:43:21 -0600

    Implement Basic IO
    Setup bot to read basic function and respond to it. Also ensure that messages are being parsed properly and adhering to 2000 char message limit.

--- lib/eigf_bot.py
+++ lib/eigf_bot.py
@@ -11,23 +11,38 @@
 
 import os
 import asyncio
-
 import discord
 
+from eigf_output.discord_outputstream import Discord_Outputstream
+
 class EIGFBot():
+    client = discord.Client()
     def __init__(self, private_token):
         self.private_token = private_token
-        self.client = discord.Client()
 
     def startup(self):
-        self.client.loop.create_task(backgroundHeartbeatTask(self.client))
-        self.client.run(self.private_token)
+        EIGFBot.client.loop.create_task(backgroundHeartbeatTask(EIGFBot.client))
+        EIGFBot.client.run(self.private_token)
+    
+    @client.event
+    async def on_ready():
+        print("EIGF Bot Ready")
+        print("Bot Name: " + EIGFBot.client.user.name)
+        print("Bot ID: " + str(EIGFBot.client.user.id))
 
+    #Need to get away from on_message and move to proper commands
+    @client.event
+    async def on_message(message):
+        parsedMessage = message.content.split(' ')
+        if(parsedMessage[0][0] == "!" and message.author != EIGFBot.client.user
+            and message.author.name + "#" + message.author.discriminator == "Champion1#5795"):
+            pass
+            
     def close(self):
         pass
 
 async def backgroundHeartbeatTask(client):
-    await client.wait_until_ready()
-    while(not client.is_closed):
+    await EIGFBot.client.wait_until_ready()
+    while(not EIGFBot.client.is_closed):
         print("Bot is still alive!")
         await asyncio.sleep(120) #Sleep for 120 seconds
--- lib/eigf_output/discord_outputstream.py
+++ lib/eigf_output/discord_outputstream.py
@@ -11,5 +11,13 @@
 
 
 class Discord_Outputstream():
-    def __init__():
-        pass
+    #Discord has a limit of 2000 characters per message for their API
+    #There is also a limit to number of messages the bot can send in a period
+    #of time. To help make the bots actions appear smoother, limiting messages
+    #may be desired.
+    @classmethod
+    async def sendMessage(self, channel, message):
+        numberOfMessageChunks = int(len(message)/2000)
+        for i in range(0, numberOfMessageChunks + 1):
+            await channel.send(message[i*2000:(i+1)*2000])
+
