ad22acde47b6f2b08d024fb011d5367216006611
RileyMKDixon <31675727+RileyMKDixon@users.noreply.github.com>
2019-05-30 10:10:17 -0600

    Setup a Player class

--- lib/eigf_player/eigf_player.py
+++ lib/eigf_player/eigf_player.py
@@ -9,6 +9,25 @@
 # v0.1 - Create stub class.
 
 class EIGFPlayer():
-    
+    self._discordPlayerName = ""
+    self._earningBonus = 0
+    self._soulEggs = 0
+    self._prophecyEggs = 0
+    self._eliteDrones = 0
+    self._regularDrones = 0
+    self._largestChickenBox = 0    
+
     def __init__():
         pass
+
+    @property
+    def discordPlayerName(self):
+        return self._DiscordPlayerName
+
+    @property
+    def earningBonus(self):
+        return self._earningBonus
+
+    @earningBonus.setter
+    def earningBonus(self, newEB):
+        pass#if 
